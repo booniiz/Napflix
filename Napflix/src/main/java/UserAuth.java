2e468ec232cdaea781174ad94e847bf6dae981b8
Ganyu Luo <ganyuluo0@gmail.com>
2020-04-17 17:04:36 -0700

    backend login

--- Napflix/src/main/java/UserAuth.java
+++ Napflix/src/main/java/UserAuth.java
@@ -3,6 +3,12 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import com.google.gson.JsonObject;
 
 @WebServlet(urlPatterns = "/ajax/login")
 public class UserAuth extends HttpServlet {
@@ -10,5 +16,33 @@ public class UserAuth extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
         String username = req.getParameter("username");
         String password = req.getParameter("password");
+        databaseAuthentication da = new databaseAuthentication();
+        JsonObject respJson = new JsonObject();
+        try{
+            Connection conn = DriverManager.getConnection(da.getAddress(),da.getUsername(), da.getPassowrd());
+            //Actual code
+            Statement statement = conn.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT *\n" +
+                    "FROM customers\n" +
+                    "WHERE email = \""+ username +"\" and password = \""+ password +"\";");
+            if (resultSet.next()){
+                System.out.println("Sucuess");
+                HttpSession session = req.getSession();
+                session.setAttribute("id",resultSet.getInt("ID"));
+                session.setAttribute("firstName", resultSet.getString("firstName"));
+                session.setAttribute("lastName", resultSet.getString("lastName"));
+                session.setAttribute("ccID", resultSet.getString("ccID"));
+                session.setAttribute("address", resultSet.getString("address"));
+                respJson.addProperty("Login", "True");
+            }else{
+                respJson.addProperty("Login", "False");
+                System.out.println("Failure");
+            }
+            resp.getWriter().write(respJson.toString());
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 }
--- Napflix/src/main/webapp/login.js
+++ Napflix/src/main/webapp/login.js
@@ -1,10 +1,24 @@
+function handleResult(data){
+    let json = JSON.parse(data);
+    if (json["Login"] == "True"){
+        window.location.replace('/Napflix')
+    }else{
+        $("#login_failed").slideDown(1000);
+    }
+}
+
 function makeAjax(){
     $.ajax({
             url: "ajax/login",
             type: "POST",
-            data: {username:$("#username").val(), password:$("#password").val()}
+            data: {username:$("#username").val(), password:$("#password").val()},
+            success: handleResult,
+            dataType: 'Text'
         }
     );
+
 }
 
 $("button").click(makeAjax);
+$("#login_failed").hide();
+$("#login_success").hide();
