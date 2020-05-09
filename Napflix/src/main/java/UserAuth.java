bdc47e0304ab3be3d93080b6ded76ba62d130c1c
boon <booniiz82@gmail.com>
2020-05-09 02:23:29 -0700

    Recaptcha test

--- Napflix/src/main/java/UserAuth.java
+++ Napflix/src/main/java/UserAuth.java
@@ -4,22 +4,38 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import com.google.gson.JsonObject;
+import java.util.HashMap;
+
+import com.google.gson.Gson;
 
 @WebServlet(urlPatterns = "/ajax/login")
 public class UserAuth extends HttpServlet {
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         String username = req.getParameter("username");
         String password = req.getParameter("password");
+        String gRecaptchaResponse = req.getParameter("g-recaptcha-response");
         System.out.println("Username: " + username);
         System.out.println("Password: " + password);
+        System.out.println("gRecaptchaResponse=" + gRecaptchaResponse);
         databaseAuthentication da = new databaseAuthentication();
-        JsonObject respJson = new JsonObject();
+        HashMap<String, String> loginInfo = new HashMap<String, String>();
+        Gson gson = new Gson();
+
+        try {
+            RecaptchaVerifyUtils.verify(gRecaptchaResponse);
+        } catch (Exception e) {
+            System.out.println(e);
+            loginInfo.put("Recaptcha","False");
+            loginInfo.put("Login","False");
+            resp.getWriter().write(gson.toJson(loginInfo));
+            return;
+        }
         try{
             Class.forName("com.mysql.jdbc.Driver").newInstance();
             Connection conn = DriverManager.getConnection(da.getAddress(),da.getUsername(), da.getPassowrd());
@@ -28,19 +44,22 @@ public class UserAuth extends HttpServlet {
                     "FROM customers\n" +
                     "WHERE email = \""+ username +"\" and password = \""+ password +"\";");
             if (resultSet.next()){
-                System.out.println("Sucuess");
+                System.out.println("Success");
                 HttpSession session = req.getSession();
                 session.setAttribute("id",resultSet.getInt("ID"));
                 session.setAttribute("firstName", resultSet.getString("firstName"));
                 session.setAttribute("lastName", resultSet.getString("lastName"));
                 session.setAttribute("ccID", resultSet.getString("ccID"));
                 session.setAttribute("address", resultSet.getString("address"));
-                respJson.addProperty("Login", "True");
-            }else{
-                respJson.addProperty("Login", "False");
+                loginInfo.put("Recaptcha","True");
+                loginInfo.put("Login","True");
+            }
+            else{
+                loginInfo.put("Recaptcha","True");
+                loginInfo.put("Login","False");
                 System.out.println("Failure");
             }
-            resp.getWriter().write(respJson.toString());
+            resp.getWriter().write(gson.toJson(loginInfo));
 
         }catch (Exception e){
             e.printStackTrace();
--- Napflix/src/main/webapp/login.js
+++ Napflix/src/main/webapp/login.js
@@ -1,9 +1,14 @@
 function handleResult(data){
     let json = JSON.parse(data);
-    if (json["Login"] == "True"){
-        //After logon, we redirect
-        window.location.replace('/Napflix/mainmenu.html')
-    }else{
+    if (json["Recaptcha"] == "True"){
+        if (json["Login"] == "True"){
+            //After logon, we redirect
+            window.location.replace('/Napflix/mainmenu.html')
+        }else{
+            $("#login_failed").slideDown(1000);
+        }
+    }
+    else{
         $("#login_failed").slideDown(1000);
     }
 }
