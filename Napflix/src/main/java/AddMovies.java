8aa4a455bba4827656a285a02eb76da1538d12b5
Ganyu Luo <ganyuluo0@gmail.com>
2020-05-12 22:10:13 -0700

    alternative implementation

--- Napflix/src/main/java/AddMovies.java
+++ Napflix/src/main/java/AddMovies.java
@@ -104,7 +104,15 @@ public class AddMovies extends HttpServlet {
                 return;
             }
             else{
-                movieID = movieID + "1";
+                try{
+                    movieID = "tt"+(Integer.parseInt(movieID.substring(2))+1);
+                }catch (Exception e){
+                    try{
+                        movieID = movieID.substring(0,3)+(Integer.parseInt(movieID.substring(3))+1);
+                    }catch (Exception ex){
+                        movieID = movieID + "1";
+                    }
+                }
                 report = "Success: Inserted movieID = " + movieID;
             }
             //-------MOVIE--------------------------------------------------------------------------------------
